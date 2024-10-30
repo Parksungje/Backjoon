f7955c30d22e20713b7097c71731f0bc426af2b4
박성제 <157565587+Parksungje@users.noreply.github.com>
2024-10-31 00:16:06 +0900

    [Bronze V] Title: 구구단, Time: 48 ms, Memory: 5812 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cs
@@ -0,0 +1,5 @@
+            int input = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{input} * {i} = {input * i}");
+            }
