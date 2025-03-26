ffd3012ba7e6c79fd5c5d195fe28c0fbc55eb21d
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-03-26 09:02:38 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.cc
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-int main(void) {
-    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-    string id;              
-    cin >> id;             
-    cout << id << "??!";   
+int main() {
+    string id;
+    cin >> id;
+    cout << id << "??!" << '\n';
 
     return 0;
 }
