5c4ff8351edab1d867437225b1399dd523addacd
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-04-03 22:47:17 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 220 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int num;
+    vector<vector<int>> v;
+    for (int i = 0; i < n; i++) {
+        vector<int> vec;
+        for (int j = 0; j < m; j++) {
+            cin >> num;
+            vec.push_back(num);
+        }
+        v.push_back(vec);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> num;
+            v[i][j] += num;
+            cout << v[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
