1a4e6b68646a20e0148aee53949e1c7447213e52
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-04-09 00:43:54 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int T;
+	int a;
+	int b;
+	cin >> T;
+
+	for (int i = 1; i <= T; i++) {
+		cin >> a >> b;
+
+		cout << "Case #" << i << ": " << a + b << "\n";
+	}
+	return 0;
+}
