3f818ad4503e372d084b53e1a8eb284416fee7f6
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-04-10 00:44:02 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int m = 0; m < i; ++m)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < n - i; ++j)
+		{
+			cout << "*";
+		}
+		cout << '\n';
+	}
+}
