63d07bb9e64957c38b182195cae6067aa156354a
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-04-11 09:08:50 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -2,33 +2,39 @@
 using namespace std;
 
 int main() {
-	int N, num, decimal=0, count=0;
+	int N, num, decimal = 0, count = 0;
 
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) 
+	{
 		cin >> num;
 
-		if (num == 2) {		// 2는 소수
+		if (num == 2) 
+		{
 			decimal++;
 		}
 		else {
-			if (num == 1) {		// 1은 소수가 아님!
+			if (num == 1) 
+			{
 				count++;
 			}
-			for (int j = 2; j < num; j++) {			
-				if (num % j == 0) {					
+			for (int j = 2; j < num; j++) 
+			{
+				if (num % j == 0) 
+				{
 					count++;
 				}
 			}
-			if (count == 0) {	// count == 0인 소수만 decimal++
+			if (count == 0) 
+			{	
 				decimal++;
 			}
-			count = 0;			//  count = 0으로 다시 초기화하기
+			count = 0;			
 		}
 	}
 
-	cout << decimal;			// decimal 출력
+	cout << decimal;			
 
 	return 0;
 }
