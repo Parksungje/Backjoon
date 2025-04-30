ee4dce6a7e2b7fc75565a1c9788aca8334c321c6
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-04-30 09:04:57 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -2,8 +2,8 @@
 #include <string>
 using namespace std;
 
-int main() {
-
+int main() 
+{
 	int n;
 	cin >> n;
 
@@ -13,17 +13,17 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> str;
 
-		if (str.length() <= 2) 
+		if (str.length() <= 2)
 		{
 			continue;
 		}
-		else 
+		else
 		{
-			for (int j = 0; j < str.length() - 1; j++) 
+			for (int j = 0; j < str.length() - 1; j++)
 			{
-				if (str[j] != str[j + 1]) 
+				if (str[j] != str[j + 1])
 				{
-					if (str.find(str[j], j + 2) != string::npos) 
+					if (str.find(str[j], j + 2) != string::npos)
 					{
 						cnt++;
 						break;
@@ -34,3 +34,4 @@ int main() {
 	}
 	cout << n - cnt;
 }
+
