18c84e1f5c8dfd5484e1603ea6921736433623af
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-05-21 23:25:32 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,30 +1,36 @@
 #include<iostream>
 #include<queue>
-#include<vector>
 using namespace std;
 
 int main()
 {
-    queue<int>nums;
-    int n, k;
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++)
-    {
-        nums.push(i);
-    }
-    cout << "<";
-    while (!nums.empty())
-    {
-        for (int i = 1; i < k; i++)
-        {
-            nums.push(nums.front());
-            nums.pop();
-        }
-        cout << nums.front();
-        nums.pop();
-        if (nums.size() != 0)
-            cout << ", ";
-        else
-            cout << ">";
-    }
+	int N, K;
+	queue<int>q;
+
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; ++i)
+	{
+		q.push(i);
+	}
+
+	cout << "<";
+	while (!q.empty())
+	{
+		for (int i = 1; i < K; ++i)
+		{
+			q.push(q.front());
+			q.pop();
+		}
+		cout << q.front();
+		q.pop();
+		if (!q.empty())
+		{
+			cout << ", ";
+		}
+		else
+		{
+			cout << ">";
+		}
+	}
 }
