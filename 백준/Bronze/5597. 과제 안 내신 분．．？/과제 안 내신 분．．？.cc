eabcae976caa5af70702e372387820d427eb6699
박성제 <157565587+Parksungje@users.noreply.github.com>
2025-07-10 09:18:53 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int arr[31] = { 0, };
+	int num = 0;
+
+	for (int i = 0; i < 28; i++)
+	{
+		cin >> num;
+		arr[num] = 1;
+	}
+
+	for (int i = 1; i <= 30; i++)
+	{
+		if (arr[i] == 0)
+		{
+			cout << i << '\n';
+		}
+	}
+
+	return 0;
+}
+
